bcf6549fc70753bb9063eb99749c82ddbdce254e
yumis56 <104646391+yumis56@users.noreply.github.com>
2024-10-16 04:40:50 -0400

    Update test-about-docker.spec.ts

--- tests/test-about-docker.spec.ts
+++ tests/test-about-docker.spec.ts
@@ -4,7 +4,8 @@ const baseUrl = 'https://yumis56.github.io/is373-project';
 const thisPostUrl = new RegExp(`${baseUrl}/\\d{4}/\\d{2}/\\d{2}/about-docker/`); // Since date changes based on re-deployment, make it a pattern. TODO change if static date is set.
 
 test.describe('About Docker Post', () => {
-  test('Verify correct link title', async ({ page }) => {
+  
+  test('Verify correct link and post text', async ({ page }) => {
     await page.goto(baseUrl); // Go to main web page
     const menuLink = await page.locator('a:has-text("About Docker")'); // Verify link labeled 'About Docker'
     await expect(menuLink).toBeVisible(); // Verify link is not hidden
@@ -20,4 +21,5 @@ test.describe('About Docker Post', () => {
     const mainHeading = await page.locator('h1.p-name.article-title'); // locate the article title element
     await expect(mainHeading).toHaveText('About Docker'); // Verify the visible header title is correct
   });
+  
 });
