b0eabb39248b659da2f087d14477819c95b6b5a4
yumis56 <104646391+yumis56@users.noreply.github.com>
2024-10-17 19:23:56 -0400

    Update test-about-12factorapp.spec.ts

--- tests/test-about-12factorapp.spec.ts
+++ tests/test-about-12factorapp.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 const baseUrl = 'https://yumis56.github.io/is373-project';
-const thisPostUrl = new RegExp(`${baseUrl}/\\d{4}/\\d{2}/\\d{2}/about-12factorapp/`); // Since date changes based on re-deployment, make it a pattern. TODO change if static date is set.
+//const thisPostUrl = new RegExp(`${baseUrl}/\\d{4}/\\d{2}/\\d{2}/about-12factorapp/`); // Since date changes based on re-deployment, make it a pattern. TODO change if static date is set.
 
 test.describe('About the Twelve Factor App Post', () => {
   
@@ -13,12 +13,19 @@ test.describe('About the Twelve Factor App Post', () => {
     /*await expect(page.locator('.widget-wrap').nth(0)).toBeVisible();*/
     /*await expect(page.locator('.widget-title')).nth(0)).toHaveText('Recent Posts');*/
 
+    const postLink = page.locator('a[href*="about-12factorapp/"]');
     
-    await expect(page.locator('a[href="/is373-project/\\d{4}/\\d{2}/\\d{2}/about-12factorapp/"]')).toBeVisible();
-    await expect(page.locator('a[href="/is373-project/\\d{4}/\\d{2}/\\d{2}/about-12factorapp/"]')).toHaveText('About the Twelve Factor App');
-    
-    await page.locator('a[href="/is373-project/\\d{4}/\\d{2}/\\d{2}/about-12factorapp/"]').click();
+    await expect(aboutLink).toBeVisible();
+    await expect(aboutLink).toHaveText('About the Twelve Factor App');
+
+    await aboutLink.click();
     await expect(page).toHaveURL(/.*about-12factorapp/);
+    
+    //await expect(page.locator('a[href="/is373-project/\\d{4}/\\d{2}/\\d{2}/about-12factorapp/"]')).toBeVisible();
+    //await expect(page.locator('a[href="/is373-project/\\d{4}/\\d{2}/\\d{2}/about-12factorapp/"]')).toHaveText('About the Twelve Factor App');
+    
+    //await page.locator('a[href="/is373-project/\\d{4}/\\d{2}/\\d{2}/about-12factorapp/"]').click();
+    //await expect(page).toHaveURL(/.*about-12factorapp/);
 
     const title = await page.title(); // get page title
 
