a61f9b4b03e443c077bde59c834aa2e33d1e28bb
yumis56 <104646391+yumis56@users.noreply.github.com>
2024-10-17 23:45:37 -0400

    Update test-about-docker.spec.ts to check for images and have proper syntax

--- tests/test-about-docker.spec.ts
+++ tests/test-about-docker.spec.ts
@@ -1,21 +1,31 @@
 import { test, expect } from '@playwright/test';
 
 const baseUrl = 'https://yumis56.github.io/is373-project';
-const thisPostUrl = new RegExp(`${baseUrl}/\\d{4}/\\d{2}/\\d{2}/about-docker/`); // Since date changes based on re-deployment, make it a pattern. TODO change if static date is set.
+//const thisPostUrl = new RegExp(`${baseUrl}/\\d{4}/\\d{2}/\\d{2}/about-docker/`); // Since date changes based on re-deployment, make it a pattern. TODO change if static date is set.
 
 test.describe('About Docker Post', () => {
   
   test('Verify correct link and post text', async ({ page }) => {
     await page.goto(baseUrl); // Go to main web page
 
-    await expect(page.locator('a[href="/is373-project/\\d{4}/\\d{2}/\\d{2}/about-docker/"]')).toBeVisible();
-    await expect(page.locator('a[href="/is373-project/\\d{4}/\\d{2}/\\d{2}/about-docker/"]')).toHaveText('About Docker');
 
-    await page.locator('a[href="/is373-project/\\d{4}/\\d{2}/\\d{2}/about-docker/"]').click();
+    await page.waitForSelector('.widget-wrap');
+    const postLink = page.locator('.widget-wrap:has(.widget-title:has-text("Recent Posts")) a[href*="about-docker"]');
+    console.log(await postLink.count()); //debug
+    
+    await expect(postLink).toBeVisible();
+    await expect(postLink).toHaveText('About Docker');
+    
+    await postLink.click();
+    
     await expect(page).toHaveURL(/.*about-docker/);
-
     const title = await page.title(); // get page title
     await expect(title).toContain('About Docker'); // just make sure post title is contained properly
+
+    const dockerLogo = page.locator('img[src="https://yumis56.github.io/is373-project/images/docker-logo.png"]');
+    await expect(dockerLogo).toBeVisible();
+    await expect(dockerLogo).toHaveAttribute('alt', 'Docker Logo');
+
 
   });
   
